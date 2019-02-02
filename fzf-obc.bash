5cbfe0ae16da2c44ebdd9f19e46006fcf8947d9a
rockandska <yoann_mac_donald@hotmail.com>
2019-02-02 23:44:56 +0100

    fix variables names

--- fzf-obc.bash
+++ fzf-obc.bash
@@ -5,46 +5,44 @@
 # /_/   /___/_/-completion.bash
 #
 # **** Not dynamic variables ****
-# - $FZF_COMPLETION_COMPAT_MODE       (default: 1)
-# - $FZF_COMPLETION_EXCLUDE           (default: empty)
-# - $FZF_COMPLETION_PATH              (default: fzf-obc/bash_completion.d)
+# - $FZF_OBC_PATH              (default: fzf-obc/bash_completion.d)
 #
 # **** All modes ****
-# - $FZF_TMUX                         (default: 0)
-# - $FZF_TMUX_HEIGHT                  (default: '40%')
-# - $FZF_COMPLETION_OPTS              (default: --select-1 --exit-0)
+# - $FZF_OBC_TMUX                         (default: 0)
+# - $FZF_OBC_TMUX_HEIGHT                  (default: '40%')
+# - $FZF_OBC_OPTS              (default: --select-1 --exit-0)
 # - $LINES                            (default: '40')
 #
-# **** Only when FZF_COMPLETION_COMPAT_MODE=0 ****
-# - $FZF_COMPLETION_MAXDEPTH          (default: 999999999)
-# - $FZF_COMPLETION_PATH_OPTS         (default: empty)
-# - $FZF_COMPLETION_DIR_OPTS          (default: empty)
+# **** Only when FZF_OBC_COMPAT_MODE=0 ****
+# - $FZF_OBC_MAXDEPTH          (default: 999999999)
+# - $FZF_OBC_PATH_OPTS         (default: empty)
+# - $FZF_OBC_DIR_OPTS          (default: empty)
 
 __fzf_obc_init_vars() {
-  : "${FZF_COMPLETION_PATH:=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/bash_completion.d}"
-  IFS=':' read -r -a FZF_COMPLETION_PATH_ARRAY <<< "${FZF_COMPLETION_PATH}"
+  : "${FZF_OBC_PATH:=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/bash_completion.d}"
+  IFS=':' read -r -a FZF_OBC_PATH_ARRAY <<< "${FZF_OBC_PATH}"
 
-  : "${FZF_TMUX:=0}"
-  : "${FZF_TMUX_HEIGHT:='40%'}"
-  : "${FZF_COMPLETION_OPTS:=--select-1 --exit-0}"
+  : "${FZF_OBC_TMUX:=0}"
+  : "${FZF_OBC_TMUX_HEIGHT:='40%'}"
+  : "${FZF_OBC_OPTS:=--select-1 --exit-0}"
   : "${LINES:=40}"
 
-  : "${FZF_COMPLETION_MAXDEPTH:=10}"
-  : "${FZF_COMPLETION_PATH_OPTS:=}"
-  : "${FZF_COMPLETION_DIR_OPTS:=}"
+  : "${FZF_OBC_MAXDEPTH:=10}"
+  : "${FZF_OBC_PATH_OPTS:=}"
+  : "${FZF_OBC_DIR_OPTS:=}"
 }
 
 ###########################################################
 
 # To use custom commands instead of find, override _fzf_compgen_{path,dir}
   _fzf_obc_files() {
-    command find -L "$1" -maxdepth "${FZF_COMPLETION_MAXDEPTH}" \
+    command find -L "$1" -maxdepth "${FZF_OBC_MAXDEPTH}" \
       -name .git -prune -o -name .svn -prune -o \( -type f -o -type l \) \
       -a -not -path "$1" -print 2> /dev/null
   }
 
   _fzf_obc_dirs() {
-    command find -L "$1" -maxdepth "${FZF_COMPLETION_MAXDEPTH}" \
+    command find -L "$1" -maxdepth "${FZF_OBC_MAXDEPTH}" \
       -name .git -prune -o -name .svn -prune -o -type d \
       -a -not -path "$1" -printf "%p/\n" 2> /dev/null
   }
@@ -55,13 +53,13 @@ __fzf_obc_init_vars() {
 bind '"\e[0n": redraw-current-line'
 
 __fzf_obc_cmd() {
-  [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX}" != 0 ] && [ ${LINES} -gt 15 ] &&
-    fzf-tmux -d${FZF_TMUX_HEIGHT} $@ || fzf $@
+  [ -n "$TMUX_PANE" ] && [ "${FZF_OBC_TMUX}" != 0 ] && [ ${LINES} -gt 15 ] &&
+    fzf-tmux -d${FZF_OBC_TMUX_HEIGHT} $@ || fzf $@
 }
 
 __fzf_obc_load() {
   local path file
-  for path in "${FZF_COMPLETION_PATH_ARRAY[@]}";do
+  for path in "${FZF_OBC_PATH_ARRAY[@]}";do
     for file in ${path}/* ; do
       [ -e "${file}" -a ! -d "${file}" ] || continue
       source "${file}"
@@ -93,9 +91,9 @@ __fzf_obc_add_trap() {
     if type -t "__fzf_obc_trap${f}" 2>&1 > /dev/null;then
       trap='__fzf_obc_trap'${f}
     fi
-    # Quit if already surcharged
+    # Quit if already surcharged with the same trap
     [[ "${origin}" =~ ${trap} ]] && return
-    # Reset trap in case is changed
+    # Reset fzf-obc params if trap changed
     origin=$(echo "${origin}" | sed -r "/(__fzf_obc_default_trap|__fzf_obc_trap${f}|fzf_original_args|fzf_defaults_opts)/d")
     local add_trap='trap '"'"''${trap}' "$?" "${fzf_original_args}"; trap - RETURN'"'"' RETURN'
     # Add trap function
@@ -140,7 +138,7 @@ __fzf_obc_default_trap() {
       IFS=$'\n' read -r -a COMPREPLY <<<$(
         printf "%s\n" "${COMPREPLY[@]}" \
         | awk '! a[$0]++' \
-        | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT} --reverse ${fzf_defaults_opts} ${FZF_COMPLETION_OPTS}" \
+        | FZF_DEFAULT_OPTS="--height ${FZF_OBC_TMUX_HEIGHT} --reverse ${fzf_defaults_opts} ${FZF_OBC_OPTS}" \
           __fzf_obc_cmd \
         | while read -r item;do printf "%q " "${item}";done \
         | sed 's/ $//'
@@ -149,7 +147,7 @@ __fzf_obc_default_trap() {
       IFS=$'\n' read -r -a COMPREPLY <<<$(
         printf "%s\n" "${COMPREPLY[@]}" \
         | awk '! a[$0]++' \
-        | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT} --reverse ${fzf_defaults_opts} ${FZF_COMPLETION_OPTS}" \
+        | FZF_DEFAULT_OPTS="--height ${FZF_OBC_TMUX_HEIGHT} --reverse ${fzf_defaults_opts} ${FZF_OBC_OPTS}" \
           __fzf_obc_cmd
       )
     fi
@@ -166,4 +164,5 @@ _fzf_complete() {
 }
 
 _fzf_complete
+
 complete -F _longopt fzf
