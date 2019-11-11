ed48b3318b4efab7be34791abcce33480b254c52
rockandska <yoann_mac_donald@hotmail.com>
2019-11-11 22:17:15 +0100

    add/fix tests for short filedir option

--- test/spec/test_cat_home.rb
+++ test/spec/test_cat_home.rb
@@ -1,86 +1,99 @@
 class FzfObcTest
   def test_cat_home
-    create_files_dirs(
-      dest: "#{temp_test_home_dir}",
-      subdirs: %w{d1 d1\ 0},
-      files: %w{
-        test-fzf-obc.test
-        test\ fzf-obc.test
-        fzf-obc.test
-      }
-    )
-    @tty.send_keys("cat #{temp_test_home_dir}/","#{TAB}",delay: 0.01)
-    @tty.send_keys("'fzf-obc.test")
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/
-      > 'fzf-obc.test
-        3/5
-      > #{temp_test_home_dir}/test-fzf-obc.test
-        #{temp_test_home_dir}/test fzf-obc.test
-        #{temp_test_home_dir}/fzf-obc.test
-    TTY
-    @tty.send_keys(<<~EOF)
-      #{CTRLC}
-      'test-fzf-obc.test
-    EOF
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/
-      > 'test-fzf-obc.test
-        1/5
-      > #{temp_test_home_dir}/test-fzf-obc.test
-    TTY
-    @tty.send_keys(<<~EOF)
-      #{CTRLC}
-      #{DOWN}
-      #{TAB}
-    EOF
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/test\\ fzf-obc.test
-    TTY
+    for short_filedir in (0..1).reverse_each do
 
-    @tty.clear_screen()
+      create_files_dirs(
+        dest: "#{temp_test_home_dir}",
+        subdirs: %w{d1 d1\ 0},
+        files: %w{
+          test-fzf-obc.test
+          test\ fzf-obc.test
+          fzf-obc.test
+        }
+      )
 
-    ################
-    # With globs
-    ################
-    @tty.send_keys("cat #{temp_test_home_dir}/**","#{TAB}",delay: 0.01)
-    @tty.send_keys("'fzf-obc.test")
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/**
-      > 'fzf-obc.test
-        9/11
-      > #{temp_test_home_dir}/test-fzf-obc.test
-        #{temp_test_home_dir}/test fzf-obc.test
-        #{temp_test_home_dir}/fzf-obc.test
-        #{temp_test_home_dir}/d1/test-fzf-obc.test
-        #{temp_test_home_dir}/d1/test fzf-obc.test
-        #{temp_test_home_dir}/d1/fzf-obc.test
-        #{temp_test_home_dir}/d1 0/test-fzf-obc.test
-        #{temp_test_home_dir}/d1 0/test fzf-obc.test
-    TTY
-    @tty.send_keys(<<~EOF)
-      #{CTRLC}
-      'test-fzf-obc.test
-    EOF
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/**
-      > 'test-fzf-obc.test
-        3/11
-      > #{temp_test_home_dir}/test-fzf-obc.test
-        #{temp_test_home_dir}/d1/test-fzf-obc.test
-        #{temp_test_home_dir}/d1 0/test-fzf-obc.test
-    TTY
-    @tty.send_keys(<<~EOF)
-      #{CTRLC}
-      'fzf-obc.test
-      #{DOWN}
-      #{TAB}
-      #{TAB}
-      #{ENTER}
-    EOF
-    @tty.assert_matches <<~TTY
-      $ cat #{temp_test_home_dir}/test\\ fzf-obc.test #{temp_test_home_dir[0..19]}
-      #{temp_test_home_dir[20..-1]}/fzf-obc.test
-    TTY
+      if short_filedir == 1
+        start_dir = ""
+      else
+        @tty.send_keys("FZF_OBC_SHORT_FILEDIR=0","#{ENTER}")
+        @tty.clear_screen()
+        start_dir = "#{temp_test_home_dir}/"
+      end
+
+      @tty.send_keys("cat #{temp_test_home_dir}/","#{TAB}",delay: 0.01)
+      @tty.send_keys("'fzf-obc.test")
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/
+        > 'fzf-obc.test
+          3/5
+        > #{start_dir}test-fzf-obc.test
+          #{start_dir}test fzf-obc.test
+          #{start_dir}fzf-obc.test
+      TTY
+      @tty.send_keys(<<~EOF)
+        #{CTRLC}
+        'test-fzf-obc.test
+      EOF
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/
+        > 'test-fzf-obc.test
+          1/5
+        > #{start_dir}test-fzf-obc.test
+      TTY
+      @tty.send_keys(<<~EOF)
+        #{CTRLC}
+        #{DOWN}
+        #{TAB}
+      EOF
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/test\\ fzf-obc.test
+      TTY
+
+      @tty.clear_screen()
+
+      ################
+      # With globs
+      ################
+      @tty.send_keys("cat #{temp_test_home_dir}/**","#{TAB}",delay: 0.01)
+      @tty.send_keys("'fzf-obc.test")
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/**
+        > 'fzf-obc.test
+          9/11
+        > #{start_dir}test-fzf-obc.test
+          #{start_dir}test fzf-obc.test
+          #{start_dir}fzf-obc.test
+          #{start_dir}d1/test-fzf-obc.test
+          #{start_dir}d1/test fzf-obc.test
+          #{start_dir}d1/fzf-obc.test
+          #{start_dir}d1 0/test-fzf-obc.test
+          #{start_dir}d1 0/test fzf-obc.test
+      TTY
+      @tty.send_keys(<<~EOF)
+        #{CTRLC}
+        'test-fzf-obc.test
+      EOF
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/**
+        > 'test-fzf-obc.test
+          3/11
+        > #{start_dir}test-fzf-obc.test
+          #{start_dir}d1/test-fzf-obc.test
+          #{start_dir}d1 0/test-fzf-obc.test
+      TTY
+      @tty.send_keys(<<~EOF)
+        #{CTRLC}
+        'fzf-obc.test
+        #{DOWN}
+        #{TAB}
+        #{TAB}
+        #{ENTER}
+      EOF
+      @tty.assert_matches <<~TTY
+        $ cat #{temp_test_home_dir}/test\\ fzf-obc.test #{temp_test_home_dir[0..19]}
+        #{temp_test_home_dir[20..-1]}/fzf-obc.test
+      TTY
+      @tty.clear_screen()
+    end
   end
 end
--- test/spec/test_vi.rb
+++ test/spec/test_vi.rb
@@ -1,86 +1,97 @@
 class FzfObcTest
   def test_vi
-    # vi use _filedir_xspec with extension filter
-    create_files_dirs(
-      subdirs: %w{d1},
-      files: %w{
-        test.conf
-        test\ 1.conf
-        test.class
-        test.mp3
-      }
-    )
-    @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
-    @tty.assert_matches(<<~EOF)
-      $ vi #{temp_test_dir}/
-      >
-        3/3
-      > #{temp_test_dir}/test.conf
-        #{temp_test_dir}/test 1.conf
-        #{temp_test_dir}/d1/
-    EOF
-    @tty.send_keys("#{DOWN}")
-    @tty.send_keys("#{TAB}")
-    @tty.assert_row(0,"$ vi #{temp_test_dir}/test\\ 1.conf")
+    for short_filedir in (0..1).reverse_each do
 
-    @tty.clear_screen()
+      # vi use _filedir_xspec with extension filter
+      create_files_dirs(
+        subdirs: %w{d1},
+        files: %w{
+          test.conf
+          test\ 1.conf
+          test.class
+          test.mp3
+        }
+      )
 
-    #############
-    # With globs
-    #############
-    @tty.send_keys("vi #{temp_test_dir}/**","#{TAB}", sleep: 0.01)
-    @tty.assert_matches(<<~EOF)
-      $ vi #{temp_test_dir}/**
-      >
-        5/5
-      > #{temp_test_dir}/test.conf
-        #{temp_test_dir}/test 1.conf
-        #{temp_test_dir}/d1/test.conf
-        #{temp_test_dir}/d1/test 1.conf
-        #{temp_test_dir}/d1/
-    EOF
-    @tty.send_keys("#{DOWN}")
-    @tty.send_keys("#{DOWN}")
-    @tty.send_keys("#{ENTER}")
-    @tty.assert_row(0,"$ vi #{temp_test_dir}/d1/test.conf")
+      if short_filedir == 1
+        start_dir = ""
+      else
+        @tty.send_keys("FZF_OBC_SHORT_FILEDIR=0","#{ENTER}")
+        @tty.clear_screen()
+        start_dir = "#{temp_test_dir}/"
+      end
 
-    @tty.clear_screen()
+      @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
+      @tty.assert_matches(<<~EOF)
+        $ vi #{temp_test_dir}/
+        >
+          3/3
+        > #{start_dir}test.conf
+          #{start_dir}test 1.conf
+          #{start_dir}d1/
+      EOF
+      @tty.send_keys("#{DOWN}")
+      @tty.send_keys("#{TAB}")
+      @tty.assert_row(0,"$ vi #{temp_test_dir}/test\\ 1.conf")
 
-    ######################
-    # Test with symlinks #
-    ######################
-    File.symlink("#{BASE}/#{temp_test_dir}/test.conf", "#{BASE}/#{temp_test_dir}/link_test.conf")
-    @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
-    @tty.assert_matches(<<~EOF)
-      $ vi #{temp_test_dir}/
-      >
-        4/4
-      > #{temp_test_dir}/test.conf
-        #{temp_test_dir}/test 1.conf
-        #{temp_test_dir}/link_test.conf
-        #{temp_test_dir}/d1/
-    EOF
-    @tty.send_keys("#{TAB}")
-    @tty.assert_row(0,"$ vi #{temp_test_dir}/test.conf")
-  
-    @tty.clear_screen()
+      @tty.clear_screen()
 
-    @tty.send_keys("rm #{temp_test_dir}/test.conf","#{ENTER}", delay: 0.01)
-    @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
-    @tty.assert_matches(<<~EOF)
-      $ rm #{temp_test_dir}/test.conf
-      $ vi #{temp_test_dir}/
-      >
-        3/3
-      > #{temp_test_dir}/test 1.conf
-        #{temp_test_dir}/link_test.conf
-        #{temp_test_dir}/d1/
-    EOF
-    @tty.send_keys("#{DOWN}")
-    @tty.send_keys("#{TAB}")
-    @tty.assert_row(1,"$ vi #{temp_test_dir}/link_test.conf")
-    @tty.send_keys("#{ENTER}")
+      #############
+      # With globs
+      #############
+      @tty.send_keys("vi #{temp_test_dir}/**","#{TAB}", sleep: 0.01)
+      @tty.assert_matches(<<~EOF)
+        $ vi #{temp_test_dir}/**
+        >
+          5/5
+        > #{start_dir}test.conf
+          #{start_dir}test 1.conf
+          #{start_dir}d1/test.conf
+          #{start_dir}d1/test 1.conf
+          #{start_dir}d1/
+      EOF
+      @tty.send_keys("#{DOWN}")
+      @tty.send_keys("#{DOWN}")
+      @tty.send_keys("#{ENTER}")
+      @tty.assert_row(0,"$ vi #{temp_test_dir}/d1/test.conf")
 
+      @tty.clear_screen()
+
+      ######################
+      # Test with symlinks #
+      ######################
+      File.symlink("#{BASE}/#{temp_test_dir}/test.conf", "#{BASE}/#{temp_test_dir}/link_test.conf")
+      @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
+      @tty.assert_matches(<<~EOF)
+        $ vi #{temp_test_dir}/
+        >
+          4/4
+        > #{start_dir}test.conf
+          #{start_dir}test 1.conf
+          #{start_dir}link_test.conf
+          #{start_dir}d1/
+      EOF
+      @tty.send_keys("#{TAB}")
+      @tty.assert_row(0,"$ vi #{temp_test_dir}/test.conf")
+    
+      @tty.clear_screen()
+
+      @tty.send_keys("rm #{temp_test_dir}/test.conf","#{ENTER}", delay: 0.01)
+      @tty.send_keys("vi #{temp_test_dir}/","#{TAB}", delay: 0.01)
+      @tty.assert_matches(<<~EOF)
+        $ rm #{temp_test_dir}/test.conf
+        $ vi #{temp_test_dir}/
+        >
+          3/3
+        > #{start_dir}test 1.conf
+          #{start_dir}link_test.conf
+          #{start_dir}d1/
+      EOF
+      @tty.send_keys("#{DOWN}")
+      @tty.send_keys("#{TAB}")
+      @tty.assert_row(1,"$ vi #{temp_test_dir}/link_test.conf")
+
+      @tty.clear_screen()
+    end
   end
-  
 end
