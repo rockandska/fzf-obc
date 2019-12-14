3b07ee53ad76161e027c84556fd9a314b100d2d9
rockandska <yoann_mac_donald@hotmail.com>
2019-12-14 01:48:11 +0100

    Add tests for plugin directory
    Remove unused __fzf_obc_finish_

--- lib/fzf-obc/finish-functions.bash
+++ lib/fzf-obc/finish-functions.bash
@@ -2,9 +2,6 @@
 
 __fzf_obc_run_finish_cmd() {
 	: "${current_cmd_name:?Missing complete command name in ${FUNCNAME[0]}}"
-	if type -t "__fzf_obc_finish_${current_func_name}" > /dev/null 2>&1;then
-		"__fzf_obc_finish_${current_func_name}" || return $?
-	fi
 	if type -t "__fzf_obc_finish_${current_cmd_name}" > /dev/null 2>&1;then
 		"__fzf_obc_finish_${current_cmd_name}" || return $?
 	fi
