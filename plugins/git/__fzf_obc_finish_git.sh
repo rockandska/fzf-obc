b9e5558ddd0566e308e4d3187fd9ab5d5f789ca5
rockandska <yoann_mac_donald@hotmail.com>
2019-12-16 19:31:06 +0100

    [git] respect subplugin enable option

--- plugins/git/__fzf_obc_finish_git.sh
+++ plugins/git/__fzf_obc_finish_git.sh
@@ -1,24 +1,26 @@
 __fzf_obc_finish_git() {
-	#############
-	# DIff view #
-	#############
-	if  [[ "${current_plugin}" == "git/diff" ]];then
-		if ((current_git_is_ref)) && ((current_fzf_multi));then
-			# Merge ref if there is two ref selected
-			if [[ "${#COMPREPLY[@]}" -eq 2 ]];then
-				__fzf_compreply < <(printf '%s..%s\0' "${COMPREPLY[0]%% }" "${COMPREPLY[1]%% }")
+	if ((current_enable));then
+		#############
+		# DIff view #
+		#############
+		if  [[ "${current_plugin}" == "git/diff" ]];then
+			if ((current_git_is_ref)) && ((current_fzf_multi));then
+				# Merge ref if there is two ref selected
+				if [[ "${#COMPREPLY[@]}" -eq 2 ]];then
+					__fzf_compreply < <(printf '%s..%s\0' "${COMPREPLY[0]%% }" "${COMPREPLY[1]%% }")
+				fi
+			elif ((current_git_is_file));then
+				__fzf_obc_git_rm_files_status
 			fi
-		elif ((current_git_is_file));then
-			__fzf_obc_git_rm_files_status
 		fi
-	fi
 
-	##############
-	# Add viewer #
-	##############
-	if  [[ "${current_plugin}" == "git/add" ]];then
-		if ((current_git_is_file));then
-			__fzf_obc_git_rm_files_status
+		##############
+		# Add viewer #
+		##############
+		if  [[ "${current_plugin}" == "git/add" ]];then
+			if ((current_git_is_file));then
+				__fzf_obc_git_rm_files_status
+			fi
 		fi
 	fi
 }
--- plugins/git/__fzf_obc_post_git.sh
+++ plugins/git/__fzf_obc_post_git.sh
@@ -5,11 +5,13 @@ __fzf_obc_post_git() {
 	# Diff viewer #
 	###############
 	if  [[ "${current_git_cmd}" == "diff" ]];then
-		__fzf_obc_git_addfiles2compreply "--others --modified --directory --no-empty-directory"
-		if ((current_git_is_ref)) || ((current_git_is_file));then
-			__fzf_obc_load_plugin_config diff
-			if ((current_git_is_file));then
-				__fzf_obc_git_add_files_status
+		__fzf_obc_load_plugin_config diff
+		if ((current_enable));then
+			__fzf_obc_git_addfiles2compreply "--others --modified --directory --no-empty-directory"
+			if ((current_git_is_ref)) || ((current_git_is_file));then
+				if ((current_git_is_file));then
+					__fzf_obc_git_add_files_status
+				fi
 			fi
 		fi
 	fi
@@ -18,9 +20,11 @@ __fzf_obc_post_git() {
 	# Add viewer #
 	##############
 	if  [[ "${current_git_cmd}" == "add" ]];then
-		if ((current_git_is_file));then
-			__fzf_obc_load_plugin_config add
-			__fzf_obc_git_add_files_status
+		__fzf_obc_load_plugin_config add
+		if ((current_enable));then
+			if ((current_git_is_file));then
+				__fzf_obc_git_add_files_status
+			fi
 		fi
 	fi
 
