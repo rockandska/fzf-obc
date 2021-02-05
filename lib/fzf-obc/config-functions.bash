85267a00991723680bff2535b5c1d24ed1871d47
rockandska <yoann_mac_donald@hotmail.com>
2021-02-05 21:33:54 +0100

    [config] add new config format functions

--- lib/fzf-obc/config-functions.bash
+++ lib/fzf-obc/config-functions.bash
@@ -3,7 +3,7 @@
 __fzf_obc_load_user_functions() {
 	local fzf_obc_path_array path file
 	IFS=':' read -r -a fzf_obc_path_array <<< "${FZF_OBC_PATH:-}"
-	for path in "${XDG_CONFIG_HOME:-$HOME/.config}/fzf-obc" "${fzf_obc_path_array[@]}";do
+	for path in "${XDG_CONFIG_HOME:-$HOME/.config}/fzf-obc"	"${fzf_obc_path_array[@]:-}";do
 		while IFS= read -r -d '' file;do
 			[[ -e "${file}" && ! -d "${file}" ]] || continue
 			# shellcheck disable=SC1090
@@ -96,4 +96,227 @@ __fzf_obc_load_config() {
 				| sed -r 's/^declare -[a-zA-Z-]+ ([^=]+)=(.*)/\1=\2;/'
 		fi
 	)
+}
+
+__fzf_obc_print_cfg2ini() {
+	# Load old config format from 1 directory and convert it to ini style
+	# $1 : directory config to print
+	#
+	# [DEFAULT]
+	# std_trigger=''
+	#
+	# [kill]
+	# std_trigger='k'
+	#
+	# [kill:plugins:process]
+	#
+	# std_trigger='p'
+
+	if [ "${#@}" -eq 0 ];then
+		__fzf_obc_error "One directory to convert needed"
+		return 1
+	elif [ "${#@}" -ne 1 ];then
+		__fzf_obc_error "Only one directory to convert is allowed"
+		return 1
+	fi
+	local start_dir
+	if [ -n "${1+x}" ];then
+		start_dir="${1}"
+	else
+		__fzf_obc_error "Missing 'start_dir' parameter"
+		return 1
+	fi
+	if ! [ -d "${start_dir}" ];then
+		__fzf_obc_error "'${start_dir}' doesn't exist or is not readable"
+		return 1
+	fi
+	local file
+	for file in "${start_dir}"/*.cfg "${start_dir}"/plugins/default.cfg	"${start_dir}"/plugins/*/*.cfg;do
+		[ -f "${file}" ] || continue
+		__fzf_obc_debug "Found configuration file: $file"
+		local short_file="${file##${start_dir}/}"
+		# Plugins config
+		local regex
+		regex="^((plugins/)([^/]+/)?)?([^/]+)\.cfg$"
+		if [[ ${short_file}  =~ $regex ]];then
+			local plugins="${BASH_REMATCH[2]%?}"
+			__fzf_obc_debug "plugins='${plugins}'"
+			local cmd="${BASH_REMATCH[3]%?}"
+			__fzf_obc_debug "cmd='${cmd}'"
+			local plugin_name="${BASH_REMATCH[4]}"
+			if [[ "${plugin_name}" == "default" ]];then
+				if [[ -z "${cmd}" ]];then
+					cmd="DEFAULT"
+					plugins="${plugins:+plugins}"
+					plugin_name=""
+				else
+					plugin_name=""
+				fi
+			fi
+			__fzf_obc_debug "plugin_name='${plugin_name}'"
+			local section
+			section="${cmd:-}"
+			section+="${plugins:+${section:+:}${plugins}}"
+			section+="${plugin_name:+${section:+:}${plugin_name}}"
+			__fzf_obc_debug "section='${section}'"
+			printf '%s\n'	"[${section}]"
+		else
+			__fzf_obc_debug "Configuration file '$file' doesn't match the filename template. ignoring...."
+		fi
+		cat "$file"
+	done
+}
+
+__fzf_obc_print_ini_config() {
+	# Get fzf-obc.ini from multiples directories and print a merge of them
+	# If fzf-obc.ini not present, will try to find old config files format
+	# $@ : directories config to lookup
+	if [ "${#@}" -lt 1 ];then
+		__fzf_obc_error 'At least one argument is requiered' 'Usage :' '__fzf_obc_get_ini_config DIRECTORIES...'
+		return 1
+	fi
+	local ini_config
+	local dir
+	for dir in "$@";do
+		if ! [ -f "${dir}/fzf-obc.ini" ];then
+			__fzf_obc_debug "'fzf-obc.ini' not found in '${dir}', try to found old config format...."
+			ini_config+=$(__fzf_obc_print_cfg2ini "${dir}")
+			ini_config+=$'\n'
+		else
+			__fzf_obc_debug "Found 'fzf-obc.icni' in '${dir}'"
+			ini_config+=$(<"${dir}/fzf-obc.ini")
+			ini_config+=$'\n'
+		fi
+	done
+
+	# Get unique sections list
+	local sections
+	IFS=$'\n' read -r -d '' -a sections < <(
+		# shellcheck disable=SC2016
+		echo "${ini_config}" \
+			| sed -r -n -e '/^\s*\[([a-zA-Z:-]+)\]\s*$/{ s/\[|\]//gp }' \
+			| LC_ALL=C sort -u
+		printf '\0'
+	)
+	local section
+	for section in "${sections[@]}";do
+		echo "[$section]"
+		echo "${ini_config}" | sed -n -r "/^\s*\[$section\]\s*$/,/^\s*\[([a-zA-Z:-]+)\]\s*$/{ //!p }"
+	done
+}
+
+__fzf_obc_print_cfg_func() {
+	# will print a function definition containing all configuration case
+	# present in the directories defined as parameter
+	if [ "${#@}" -eq 0 ];then
+		1>&2 echo 'At least one directory where to search configuration is required'
+		return 1
+	fi
+	printf '%s\n' '__fzf_obc_cfg_get() {'
+	cat <<- 'EOF'
+		# __fzf_obc_cfg_get [trigger] [option] [cmd] [plugin]
+		# __fzf_obc_cfg_get std fzf_trigger kill process
+		local return_var="${1:-}"
+		local trigger="${2:-}"
+		local option="${3:-}"
+		local cmd="${4:-}"
+		local plugin="${5:-}"
+
+		if [ "${#@}" -eq 0 ];then
+			__fzf_obc_error "__fzf_obc_cfg_get RETURN_VAR TRIGGER OPTION [cmd] [plugin]"
+			return 1
+		fi
+		if [ -z "${return_var}" ];then
+			__fzf_obc_error "Missing 'return_var' parameter"
+			return 1
+		fi
+		if [ -z "${trigger}" ];then
+			__fzf_obc_error "Missing 'trigger' parameter"
+			return 1
+		fi
+		if [ -z "${option}" ];then
+			__fzf_obc_error "Missing 'option' parameter"
+			return 1
+		fi
+
+		# Declare local variables by trigger type
+		# Standard, Multi selection, Recursive
+		local trigger_type_arr=(
+			"std"
+			"mlt"
+			"rec"
+		)
+
+		# Declare all options type
+		local options_type_arr=(
+			"enable"
+			"fzf_trigger"
+			"fzf_multi"
+			"fzf_opts"
+			"fzf_binds"
+			"fzf_size"
+			"fzf_position"
+			"fzf_tmux"
+			"fzf_colors"
+			"sort_opts"
+			"filedir_short"
+			"filedir_colors"
+			"filedir_hidden_first"
+			"filedir_maxdepth"
+			"filedir_exclude_path"
+		)
+
+		# loop to declare all variables as local
+		# local [trigger_type]_[options_type]
+		local x y
+		for x in "${trigger_type_arr[@]}";do
+			for y in "${options_type_arr[@]}";do
+				eval "local ${x}_${y}="
+			done
+		done
+
+		# Define loading order (lower first)
+		local cfg2test
+		cfg2test=("DEFAULT")
+		if [ -n "${cmd:-}" ];then
+			cfg2test+=("${cmd:-}")
+		fi
+		if [ -n "${cmd:-}" ] && [ -n "${plugin:-}" ];then
+			cfg2test+=("DEFAULT:plugins" "${cmd:-}:plugins" "${cmd:-}:plugins:${plugin:-}")
+		fi
+		local cfg_level
+		for cfg_level in "${cfg2test[@]}";do
+			case "${cfg_level}" in
+	EOF
+	# Transforming ini file in bash case style
+	# shellcheck disable=SC2016
+	sed -n -r -e '/^\s*\[.*\]\s*$/{s/\[//;s/\]/\)/;:a' -e 's/\[/;;\n/;s/\]/\)/' -e '$!N;$!ba' -e '}' -e '${s/$/\n;;/}' -e 'p' <(__fzf_obc_print_ini_config "$@")
+	cat <<- 'EOF'
+			esac
+		done
+
+		local option2display="${trigger}_${option}"
+
+		# Try FZF_OBC[trigger_option] env
+		local env_var
+		env_var="$( echo "FZF_OBC_${option2display}" | tr a-z A-Z)"
+		if [[ -n "${!env_var+x}" ]];then
+			eval "${option2display}=\"${!env_var}\""
+		fi
+
+		# specific case
+		case "${option2display:-}" in
+			${trigger}_filedir_colors)
+				[[ -n "${LS_COLORS:-}" ]] || eval "${trigger}_filedir_colors=0"
+				;;
+		esac
+
+		if [ -n "${!option2display+x}" ];then
+			printf -v "${return_var}" '%s' "${!option2display:-}"
+		else
+			__fzf_obc_error "Unknown option '${option2display}'"
+			return 1
+		fi
+	}
+	EOF
 }
---
+++ test/bats/helpers/common.bash
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2034
+if [ -z "${BATS_PROJECT_DIR+x}" ];then
+	1>&2 echo "Error: BATS_PROJECT_DIR is not set"
+	return 1
+fi
+
+set -Eeuo pipefail
+
+register_env() {
+	printf -v "${1:-BATS_PREVIOUS_ENV}" '%s\n' "$(compgen -v | sort | while read -r var; do printf "%s=%q\n" "$var" "${!var:-}"; done)"
+}
+
+assert_env_clean() {
+	local _BATS_PREVIOUS_ENV
+	register_env _BATS_PREVIOUS_ENV
+	local pattern
+	pattern=("$@")
+	pattern+=(
+		BASHPID
+		"BASH_.*"
+		"BATS_.*"
+		"bats_.*"
+		FUNCNAME
+		LINENO
+		lines
+		output
+		RANDOM
+		SECONDS
+		status
+		"_.*"
+		exclude_paths
+		path
+	)
+	local regex
+	regex='^(declare -- .. )?('
+	regex+=$(printf '|%s' "${pattern[@]}")
+	regex+=')='
+	local out
+	if ! out=$(diff <(printf '%s\n' "${BATS_PREVIOUS_ENV:-}"  | grep -Ev "${regex}") <(printf '%s\n' "${_BATS_PREVIOUS_ENV}" | grep -Ev "${regex}"));then
+		1>&2 printf -- '%s\n' '-----' 'Env is not clean, see diff bellow' '-----'
+		1>&2 printf '%s\n' "$out"
+		return 1
+	fi
+}
+
+run_helper() {
+	"$@"
+}
+
+run() {
+	local origFlags
+	origFlags="$(set +o); set -${-//c}"
+	local origIFS="$IFS"
+	local previous_trap
+	printf -v "previous_trap" '%s\n' "$(trap)"
+	local stdout_file stderr_file
+	stdout_file=$(mktemp "${BATS_RUN_TMPDIR}/stdout.XXXXXX")
+	stderr_file=$(mktemp "${BATS_RUN_TMPDIR}/stderr.XXXXXX")
+	local rc
+	trap 'rc=$?; trap - ERR;return 0' ERR
+	run_helper "$@" 1> "${stdout_file}" 2> "${stderr_file}"
+	eval "${previous_trap:-}"
+	#shellcheck disable=SC2034
+	status="${rc:-0}"
+	local output_sep="${output_sep-$'\n'}"
+	##### bats_stdout / bats_stdout_lines
+	output=""
+	lines=()
+	bats_stdout=""
+	bats_stdout_lines=()
+	local line
+	while IFS= read -r -d $'\n' line || [[ $line ]]; do
+		printf -v  output "%s\n" "${output:-}${line}"
+		printf -v  bats_stdout "%s\n" "${bats_stdout:-}${line}"
+		bats_stdout_lines+=("${line}")
+		lines+=("${line}")
+	done < "${stdout_file:-}"
+	bats_stdout="${bats_stdout%?}"
+	##### bats_stderr / bats_stderr_lines
+	bats_stderr=""
+	bats_stderr_lines=()
+	while IFS= read -r -d $'\n' line || [[ $line ]]; do
+		printf -v  output "%s\n" "${output:-}${line}"
+		printf -v  bats_stderr "%s\n" "${bats_stderr:-}${line}"
+		bats_stderr_lines+=("${line}")
+		lines+=("${line}")
+	done < "${stderr_file:-}"
+	bats_stderr="${bats_stderr%?}"
+	output="${output%?}"
+	#####
+	IFS="$origIFS"
+	eval "${origFlags}"
+}
+
+unset file
+register_env
